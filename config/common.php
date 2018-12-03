33abf56b68f49b9e5c4b16745b46602e02008747
Andrii Vasyliev <sol@hiqdev.com>
2018-12-03 14:52:56 +0000

    Added DI config for cache interfaces

--- config/common.php
+++ config/common.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
+    \Psr\SimpleCache\CacheInterface::class => \yii\di\Reference::to('cache'),
+    \yii\cache\CacheInterface::class => \yii\di\Reference::to('cache'),
     'cache' => [
-        '__class' => yii\cache\Cache::class,
+        '__class' => \yii\cache\Cache::class,
     ],
 ];
