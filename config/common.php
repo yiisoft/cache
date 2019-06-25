ded526b2c8e9b572d2420870581b283cef4ce58c
razonyang <razonyang@gmail.com>
2019-06-25 19:31:57 +0800

    Adjust to yiisoft/di changes

--- config/common.php
+++ config/common.php
@@ -1,8 +1,10 @@
 <?php
 
+use Yiisoft\Factory\Definitions\Reference;
+
 return [
-    \Psr\SimpleCache\CacheInterface::class => \yii\di\Reference::to('cache'),
-    \Yiisoft\Cache\CacheInterface::class => \yii\di\Reference::to('cache'),
+    \Psr\SimpleCache\CacheInterface::class => Reference::to('cache'),
+    \Yiisoft\Cache\CacheInterface::class => Reference::to('cache'),
     'cache' => [
         '__class' => \Yiisoft\Cache\Cache::class,
     ],
