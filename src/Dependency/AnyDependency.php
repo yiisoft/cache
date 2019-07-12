847e1abcd355257b95d37f977e01be0a82b97397
Alexander Makarov <sam@rmcreative.ru>
2019-07-12 11:56:18 +0000

    Apply fixes from StyleCI

--- src/Dependency/AnyDependency.php
+++ src/Dependency/AnyDependency.php
@@ -16,38 +16,38 @@ class AnyDependency extends Dependency
     /**
          * @var Dependency[]
          */
-        private $dependencies;
+    private $dependencies;
 
-        /**
-         * ChainedDependency constructor.
-         * @param Dependency[] $dependencies list of dependencies that this dependency is composed of.
-         * Each array element must be a dependency object.
-         */
-        public function __construct(array $dependencies = [])
-        {
-            $this->dependencies = $dependencies;
+    /**
+     * ChainedDependency constructor.
+     * @param Dependency[] $dependencies list of dependencies that this dependency is composed of.
+     * Each array element must be a dependency object.
+     */
+    public function __construct(array $dependencies = [])
+    {
+        $this->dependencies = $dependencies;
+    }
+
+    public function evaluateDependency(CacheInterface $cache): void
+    {
+        foreach ($this->dependencies as $dependency) {
+            $dependency->evaluateDependency($cache);
         }
+    }
 
-        public function evaluateDependency(CacheInterface $cache): void
-        {
-            foreach ($this->dependencies as $dependency) {
-                $dependency->evaluateDependency($cache);
+    protected function generateDependencyData(CacheInterface $cache)
+    {
+        return null;
+    }
+
+    public function isChanged(CacheInterface $cache): bool
+    {
+        foreach ($this->dependencies as $dependency) {
+            if ($dependency->isChanged($cache)) {
+                return true;
             }
         }
 
-        protected function generateDependencyData(CacheInterface $cache)
-        {
-            return null;
-        }
-
-        public function isChanged(CacheInterface $cache): bool
-        {
-            foreach ($this->dependencies as $dependency) {
-                if ($dependency->isChanged($cache)) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
+        return false;
+    }
 }
