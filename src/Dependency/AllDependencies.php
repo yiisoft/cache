158e395ca5a41e55f1406989400ee2006b77e5b1
Alexander Makarov <sam@rmcreative.ru>
2019-07-12 14:56:02 +0300

    Split ChainedDependency into AnyDependency and AllDependencies, add tests

---
+++ src/Dependency/AllDependencies.php
@@ -0,0 +1,52 @@
+<?php
+namespace Yiisoft\Cache\Dependency;
+
+use Yiisoft\Cache\CacheInterface;
+
+/**
+ * AllDependencies represents a dependency which is composed of a list of other dependencies.
+ *
+ * The dependency is reported as changed if all sub-dependencies are changed.
+ *
+ * For more details and usage information on Cache, see the [guide article on caching](guide:caching-overview).
+ */
+class AllDependencies extends Dependency
+{
+    /**
+     * @var Dependency[]
+     */
+    private $dependencies;
+
+    /**
+     * ChainedDependency constructor.
+     * @param Dependency[] $dependencies list of dependencies that this dependency is composed of.
+     * Each array element must be a dependency object.
+     */
+    public function __construct(array $dependencies = [])
+    {
+        $this->dependencies = $dependencies;
+    }
+
+    public function evaluateDependency(CacheInterface $cache): void
+    {
+        foreach ($this->dependencies as $dependency) {
+            $dependency->evaluateDependency($cache);
+        }
+    }
+
+    protected function generateDependencyData(CacheInterface $cache)
+    {
+        return null;
+    }
+
+    public function isChanged(CacheInterface $cache): bool
+    {
+        foreach ($this->dependencies as $dependency) {
+            if (!$dependency->isChanged($cache)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/Dependency/AnyDependency.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Yiisoft\Cache\Dependency;
+
+use Yiisoft\Cache\CacheInterface;
+
+/**
+ * AnyDependency represents a dependency based on the result of a callback.
+ *
+ * The dependency is reported as changed if any sub-dependency is changed.
+ *
+ * For more details and usage information on Cache, see the [guide article on caching](guide:caching-overview).
+ */
+class AnyDependency extends Dependency
+{
+    /**
+         * @var Dependency[]
+         */
+        private $dependencies;
+
+        /**
+         * ChainedDependency constructor.
+         * @param Dependency[] $dependencies list of dependencies that this dependency is composed of.
+         * Each array element must be a dependency object.
+         */
+        public function __construct(array $dependencies = [])
+        {
+            $this->dependencies = $dependencies;
+        }
+
+        public function evaluateDependency(CacheInterface $cache): void
+        {
+            foreach ($this->dependencies as $dependency) {
+                $dependency->evaluateDependency($cache);
+            }
+        }
+
+        protected function generateDependencyData(CacheInterface $cache)
+        {
+            return null;
+        }
+
+        public function isChanged(CacheInterface $cache): bool
+        {
+            foreach ($this->dependencies as $dependency) {
+                if ($dependency->isChanged($cache)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+}
