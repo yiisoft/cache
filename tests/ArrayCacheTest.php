eded721f6fc7f200d5b113727ce6171ff684f847
Alexander Makarov <sam@rmcreative.ru>
2019-07-15 10:43:29 +0300

    Added has() checks to ArrayCacheTest

--- tests/ArrayCacheTest.php
+++ tests/ArrayCacheTest.php
@@ -22,9 +22,13 @@ class ArrayCacheTest extends CacheTest
 
         static::$time = \time();
         $this->assertTrue($cache->set('expire_test', 'expire_test', 2));
+
         static::$time++;
+        $this->assertTrue($cache->has('expire_test'));
         $this->assertEquals('expire_test', $cache->get('expire_test'));
+
         static::$time++;
+        $this->assertFalse($cache->has('expire_test'));
         $this->assertNull($cache->get('expire_test'));
     }
 
@@ -34,9 +38,13 @@ class ArrayCacheTest extends CacheTest
 
         static::$time = \time();
         $this->assertTrue($cache->add('expire_testa', 'expire_testa', 2));
+
         static::$time++;
+        $this->assertTrue($cache->has('expire_testa'));
         $this->assertEquals('expire_testa', $cache->get('expire_testa'));
+
         static::$time++;
+        $this->assertFalse($cache->has('expire_testa'));
         $this->assertNull($cache->get('expire_testa'));
     }
 }
