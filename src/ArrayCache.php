107155a686c70264307f4f978cb8565706e012e7
Alexander Kartavenko <askemailbox@gmail.com>
2019-08-08 15:29:20 +0300

    Remove BaseCache

--- src/ArrayCache.php
+++ src/ArrayCache.php
@@ -2,13 +2,20 @@
 
 namespace Yiisoft\Cache;
 
+use DateInterval;
+use DateTime;
+use Exception;
+use Psr\SimpleCache\CacheInterface;
+
 /**
  * ArrayCache provides caching for the current request only by storing the values in an array.
  *
  * See {@see \Psr\SimpleCache\CacheInterface} for common cache operations that ArrayCache supports.
  */
-final class ArrayCache extends BaseCache
+final class ArrayCache implements CacheInterface
 {
+    public const EXPIRATION_INFINITY = 0;
+
     private $cache = [];
 
     public function get($key, $default = null)
@@ -89,5 +96,43 @@ final class ArrayCache extends BaseCache
     private function isExpired($key): bool
     {
         return $this->cache[$key][1] !== 0 && $this->cache[$key][1] <= time();
+    }
+
+    /**
+     * Converts TTL to expiration
+     * @param $ttl
+     * @return int
+     */
+    protected function ttlToExpiration($ttl): int
+    {
+        $ttl = $this->normalizeTtl($ttl);
+
+        if ($ttl === null) {
+            $expiration = static::EXPIRATION_INFINITY;
+        } elseif ($ttl <= 0) {
+            $expiration = -1;
+        } else {
+            $expiration = $ttl + time();
+        }
+
+        return $expiration;
+    }
+
+    /**
+     * Normalizes cache TTL handling `null` value and {@see DateInterval} objects.
+     * @param int|DateInterval|null $ttl raw TTL.
+     * @return int|null TTL value as UNIX timestamp or null meaning infinity
+     */
+    protected function normalizeTtl($ttl): ?int
+    {
+        if ($ttl instanceof DateInterval) {
+            try {
+                return (new DateTime('@0'))->add($ttl)->getTimestamp();
+            } catch (Exception $e) {
+                return null;
+            }
+        }
+
+        return $ttl;
     }
 }
