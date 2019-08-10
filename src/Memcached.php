85e1a2203955ace97e9b25e582053352118471b1
Alexander Kartavenko <askemailbox@gmail.com>
2019-08-10 17:57:16 +0300

    Make constants private

--- src/Memcached.php
+++ src/Memcached.php
@@ -22,10 +22,10 @@ use Yiisoft\Cache\Exception\InvalidConfigException;
  */
 final class Memcached implements CacheInterface
 {
-    public const EXPIRATION_INFINITY = 0;
-    public const DEFAULT_SERVER_HOST = '127.0.0.1';
-    public const DEFAULT_SERVER_PORT = 11211;
-    public const DEFAULT_SERVER_WEIGHT = 1;
+    private const EXPIRATION_INFINITY = 0;
+    private const DEFAULT_SERVER_HOST = '127.0.0.1';
+    private const DEFAULT_SERVER_PORT = 11211;
+    private const DEFAULT_SERVER_WEIGHT = 1;
 
     /**
      * @var \Memcached the Memcached instance
