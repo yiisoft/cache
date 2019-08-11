38178e5779af079ea63a8b9d718ad4c81f9d363d
Alexander Kartavenko <askemailbox@gmail.com>
2019-08-11 21:17:40 +0300

    travis

--- tests/travis/apcu-setup.sh
+++ tests/travis/apcu-setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -e
 
-sudo apt -y update
-sudo apt -y install php$(phpenv version-name)-apcu
+yes '' | pecl install -f apcu
 echo "apc.enabled = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
