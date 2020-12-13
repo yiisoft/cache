d4c7c4ab3364108e04ab6936df6bc5d4c2af6946
devanych <mail@devanych.ru>
2020-12-13 17:01:48 +0300

    Change NullCache to PSR-16 implementation

--- tests/NullCacheTest.php
+++ tests/NullCacheTest.php
@@ -4,87 +4,62 @@ declare(strict_types=1);
 
 namespace Yiisoft\Cache\Tests;
 
+use Yiisoft\Cache\Cache;
 use Yiisoft\Cache\NullCache;
 
 class NullCacheTest extends TestCase
 {
-    private function getCache(): NullCache
-    {
-        return new NullCache();
-    }
+    private NullCache $cache;
 
-    public function testAdd(): void
+    public function setUp(): void
     {
-        $this->assertTrue($this->getCache()->add('key', 42));
-    }
-
-    public function testDeleteMultiple(): void
-    {
-        $this->assertTrue($this->getCache()->deleteMultiple(['a', 'b']));
-    }
-
-    public function testSet(): void
-    {
-        $this->assertTrue($this->getCache()->set('key', 42));
+        $this->cache = new NullCache();
     }
 
     public function testGet(): void
     {
-        $this->assertSame(42, $this->getCache()->get('key', 42));
+        $this->assertSame(42, $this->cache->get('key', 42));
     }
 
-    public function testGetMultiple(): void
+    public function testSet(): void
     {
-        $this->assertSame(['a' => 42, 'b' => 42], $this->getCache()->getMultiple(['a', 'b'], 42));
-    }
-
-    public function testSetMultiple(): void
-    {
-        $this->assertTrue($this->getCache()->setMultiple(['a' => 42]));
-    }
-
-    public function testAddMultiple(): void
-    {
-        $this->assertTrue($this->getCache()->addMultiple(['a' => 42]));
-    }
-
-    public function testGetOrSet(): void
-    {
-        $this->assertSame(42, $this->getCache()->getOrSet('key', static function () {
-            return 42;
-        }));
+        $this->assertTrue($this->cache->set('key', 42));
     }
 
     public function testDelete(): void
     {
-        $this->assertTrue($this->getCache()->delete('key'));
+        $this->assertTrue($this->cache->delete('key'));
     }
 
     public function testClear(): void
     {
-        $this->assertTrue($this->getCache()->clear());
+        $this->assertTrue($this->cache->clear());
+    }
+
+    public function testGetMultiple(): void
+    {
+        $this->assertSame(['a' => 42, 'b' => 42], $this->cache->getMultiple(['a', 'b'], 42));
+    }
+
+    public function testSetMultiple(): void
+    {
+        $this->assertTrue($this->cache->setMultiple(['a' => 42]));
+    }
+
+    public function testDeleteMultiple(): void
+    {
+        $this->assertTrue($this->cache->deleteMultiple(['a', 'b']));
     }
 
     public function testHas(): void
     {
-        $this->assertFalse($this->getCache()->has('key'));
+        $this->assertFalse($this->cache->has('key'));
     }
 
-    public function testEnableKeyNormalization(): void
+    public function testCreateCacheAndGetOrSet(): void
     {
-        $this->getCache()->enableKeyNormalization();
-        $this->assertTrue(true);
-    }
-
-    public function testDisableKeyNormalization(): void
-    {
-        $this->getCache()->disableKeyNormalization();
-        $this->assertTrue(true);
-    }
-
-    public function testSetKeyPrefix(): void
-    {
-        $this->getCache()->setKeyPrefix('prefix');
-        $this->assertTrue(true);
+        $this->assertSame(42, (new Cache($this->cache))->getOrSet('key', static function () {
+            return 42;
+        }));
     }
 }
