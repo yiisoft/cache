a96cc0ff64926177fe158e798beec029a894125f
devanych <mail@devanych.ru>
2020-12-14 14:40:54 +0300

    Refactoring SetCacheException

--- src/Exception/SetCacheException.php
+++ src/Exception/SetCacheException.php
@@ -4,39 +4,20 @@ declare(strict_types=1);
 
 namespace Yiisoft\Cache\Exception;
 
+use Yiisoft\Cache\Metadata\CacheItem;
+
 final class SetCacheException extends CacheException
 {
-    /**
-     * @var mixed
-     */
-    private $value;
-    private ?int $ttl;
+    private CacheItem $item;
 
-    /**
-     * @param string $key
-     * @param mixed $value
-     * @param int|null $ttl
-     */
-    public function __construct(string $key, $value, ?int $ttl)
+    public function __construct(string $key, CacheItem $item)
     {
-        $this->value = $value;
-        $this->ttl = $ttl;
+        $this->item = $item;
         parent::__construct($key, 'Failed to store the value in the cache.');
     }
 
-    /**
-     * @return mixed
-     */
-    public function getValue()
+    public function getItem(): CacheItem
     {
-        return $this->value;
-    }
-
-    /**
-     * @return int|null
-     */
-    public function getTtl(): ?int
-    {
-        return $this->ttl;
+        return $this->item;
     }
 }
