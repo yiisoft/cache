eb2d2500c72449e7eeed78550593def2d0ea51f8
devanych <mail@devanych.ru>
2020-12-18 21:16:15 +0300

    Add ValueDependency

---
+++ src/Dependency/ValueDependency.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Cache\Dependency;
+
+use Yiisoft\Cache\CacheInterface;
+
+/**
+ * ValueDependency represents a dependency based on the specified value in the constructor.
+ *
+ * The dependency is reported as unchanged if and only if the specified value is
+ * the same as the one evaluated when storing the data to cache.
+ */
+final class ValueDependency extends Dependency
+{
+    /**
+     * @var mixed
+     */
+    private $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param CacheInterface $cache
+     *
+     * @return mixed
+     */
+    protected function generateDependencyData(CacheInterface $cache)
+    {
+        return $this->value;
+    }
+}
