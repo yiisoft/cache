94bb931d667a7290e77a4612dd82e01c4f0d8fd4
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-02-15 17:28:18 +0300

    Adapt configuration group names to Yii conventions (#117)

---
+++ tests/ConfigTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Cache\Tests;
+
+use Yiisoft\Cache\ArrayCache;
+use Yiisoft\Cache\Cache;
+use Yiisoft\Di\Container;
+use Yiisoft\Di\ContainerConfig;
+
+final class ConfigTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $container = $this->createContainer();
+
+        $yiiCache = $container->get(\Yiisoft\Cache\CacheInterface::class);
+        $psrCache = $container->get(\Psr\SimpleCache\CacheInterface::class);
+
+        $this->assertInstanceOf(Cache::class, $yiiCache);
+        $this->assertInstanceOf(ArrayCache::class, $psrCache);
+    }
+
+    private function createContainer(): Container
+    {
+        return new Container(
+            ContainerConfig::create()->withDefinitions(
+                $this->getDiConfig()
+            )
+        );
+    }
+
+    private function getDiConfig(): array
+    {
+        return require dirname(__DIR__) . '/config/di.php';
+    }
+}
